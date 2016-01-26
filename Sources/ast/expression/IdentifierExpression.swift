d366a062868080b5ac0e37c9fff257634706e062
Ryuichi Saito <ryuichi@ryuichisaito.com>
2016-01-25 21:36:24 -0800

    Make Identifier a typealias to String

--- Sources/ast/expression/IdentifierExpression.swift
+++ Sources/ast/expression/IdentifierExpression.swift
@@ -14,16 +14,18 @@
    limitations under the License.
 */
 
+public typealias Identifier = String // TODO: identifier will have its own dedicated class when it becomes more complicated
+
 public class IdentifierExpression: PrimaryExpression {
-    public let identifier: String
+    public let identifier: Identifier
     public let generic: GenericArgumentClause?
 
-    public init(identifier: String, generic: GenericArgumentClause?) {
+    public init(identifier: Identifier, generic: GenericArgumentClause?) {
         self.identifier = identifier
         self.generic = generic
     }
 
-    public convenience init(identifier: String) {
+    public convenience init(identifier: Identifier) {
         self.init(identifier: identifier, generic: nil)
     }
 }
