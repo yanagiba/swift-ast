d86d9a1d6b992a566a4a025a132fde1ad38be525
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-05-22 07:24:21 -0500

    Show tool version in terminal output

---
+++ Sources/AST/Version.swift
@@ -0,0 +1,22 @@
+/*
+   Copyright 2017 Ryuichi Saito, LLC and the Yanagiba project contributors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+public struct Version {
+  public let library: String
+  public let swift: String
+
+  public static let current = Version(library: "0.1.4", swift: "3.1")
+}
