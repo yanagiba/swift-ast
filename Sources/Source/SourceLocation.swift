40d4d71fbcdf66945c510c72094a9c8d54a1acf7
Ryuichi Saito <ryuichi@ryuichisaito.com>
2017-07-10 10:59:37 -0500

    Rename SourceLocation.path to SourceLocation.identifier

--- Sources/Source/SourceLocation.swift
+++ Sources/Source/SourceLocation.swift
@@ -15,12 +15,24 @@
 */
 
 public struct SourceLocation {
-  public let path: String
+  public let identifier: String
   public let line: Int
   public let column: Int
 
+  @available(*, deprecated, message: "Use `identifier` instead.")
+  public var path: String {
+    return identifier
+  }
+
+  public init(identifier: String, line: Int, column: Int) {
+    self.identifier = identifier
+    self.line = line
+    self.column = column
+  }
+
+  @available(*, deprecated, message: "Use `init(identifier:line:column:)` instead.")
   public init(path: String, line: Int, column: Int) {
-    self.path = path
+    self.identifier = path
     self.line = line
     self.column = column
   }
