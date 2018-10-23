295998ce223da98cab6b7189db22f0be3dfc0eb7
Ryuichi Sai <ryuichi@ryuichisai.com>
2018-10-23 17:19:51 -0500

    Update version

--- Sources/AST/Version.swift
+++ Sources/AST/Version.swift
@@ -18,5 +18,5 @@ public struct Version {
   public let library: String
   public let swift: String
 
-  public static let current = Version(library: "0.4.4", swift: "4.2")
+  public static let current = Version(library: "0.18.10", swift: "4.2")
 }
